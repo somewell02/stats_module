60f998c84ea9b3a2a9fcfb6c29542054c0cfedc3
Samvel <samvel121202@mail.ru>
2023-05-09 13:42:30 +0300

    Init project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import router from "./router";
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(router).use(pinia);
+app.mount("#app");
